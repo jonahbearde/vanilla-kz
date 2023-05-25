d626cac025bc89ee703ce5bca155b17b1a212d53
Reeed <gniqz@foxmail.com>
2023-05-25 11:24:19 +0800

    fix

--- src/components/stats/Info.vue
+++ src/components/stats/Info.vue
@@ -6,7 +6,7 @@
 		<div v-else class="flex flex-col sm:flex-row gap-2 sm:gap-6 items-center">
 			<!-- TODO show avatar? -->
 			<a :href="props.steamUser.profileurl" target="_blank">
-				<img :src="props.steamUser.avatar" class="w-[60px] h-auto border-2 border-slate-400 dark:border-black rounded-sm sm:ml-6">
+				<img :src="props.steamUser.avatarfull" class="w-[60px] h-auto border-2 border-slate-400 dark:border-black rounded-sm sm:ml-6">
 			</a>
 			<p class="text-3xl text-[#00b894] font-bold">{{ info?.name }}</p>
 			<img v-if="props.steamUser.loccountrycode" :src="'https://flagcdn.com/28x21/' + props.steamUser.loccountrycode.toLowerCase() + '.png'">
