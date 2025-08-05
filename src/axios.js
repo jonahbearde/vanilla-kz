ec1fc56f403bca9dc5bcfe964584d53aa4afa1b9
jonabirde <bearinparadox@gmail.com>
2025-08-05 17:52:41 +0800

    add access token to request header

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 import { vnlApiBaseUrl } from './constants'
+import useUserStore from './store/user'
 
+const userStore = useUserStore()
 
 const axiosClient = axios.create({
 	baseURL: vnlApiBaseUrl
 })
 
 axiosClient.interceptors.request.use(config => {
-	config.headers.Authorization = `Bearer `
+	config.headers.Authorization = `Bearer ${userStore.user.token}`
 	return config
 })
 
