222138f9c90e5cc7c6cee8d6b558049d02914969
jonabirde <bearinparadox@gmail.com>
2025-08-05 18:41:43 +0800

    get token from local storage instead

--- src/axios.js
+++ src/axios.js
@@ -1,18 +1,14 @@
-import axios from 'axios'
-import { vnlApiBaseUrl } from './constants'
-import useUserStore from './store/user'
-
-const userStore = useUserStore()
+import axios from "axios"
+import { vnlApiBaseUrl } from "./constants"
 
 const axiosClient = axios.create({
-	baseURL: vnlApiBaseUrl
+  baseURL: vnlApiBaseUrl
 })
 
-axiosClient.interceptors.request.use(config => {
-	config.headers.Authorization = `Bearer ${userStore.user.token}`
-	return config
+axiosClient.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"))
+  config.headers.Authorization = `Bearer ${user.token}`
+  return config
 })
 
 export default axiosClient
-
-
